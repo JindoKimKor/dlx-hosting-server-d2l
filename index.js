43c1eb629ca060e82bc0f7d52a2b9a0123d17b32
Continuous Integration Catherine <varlabcc@gmail.com>
2024-08-13 18:14:36 +0000

    Merged in Photogrammetry (pull request #8)
    added redirect for new folder
    
    * added redirect for new folder

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ lti.setup(
         `${process.env.URL}/dlx/Paramedic-Ambulance`,
         `${process.env.URL}/dlx/LTI-Package-Test`,
         `${process.env.URL}/dlx/EcoQuorum`,
+        `${process.env.URL}/dlx/Photogrammetry`
       ],
       autoActivate: true,
     },
